a65011f53da8f1333878eded3f480775be10f2c8
Roman Kutselepa <roman.kutselepa@gmail.com>
2024-01-09 12:08:08 +0200

    first commit

---
+++ hw1_foreach.js
@@ -0,0 +1,49 @@
+// Напишите программу которая сложит все числа в заданном массиве
+// выведет результат в консоль
+const nums1 = [5, 6, 92, 47, 12, -18, 33, 8];
+
+
+// Напишите программу которая добавит в массив namesObject объекты
+// с двумя парами { name: "имя с большой буквы", nameLength: "длина имени"}
+const names = ['jack', 'sarah', 'mary', 'joey', 'chris', 'samantha'];
+const namesObject = [];
+
+
+// Напишите программу которая в массив numsRes добавит объекты
+// с тремя парами { number: "само число", square: "число в квадрпате", cube: "число в кубе"}
+const nums2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const numsRes = [];
+
+
+// Используйте forEach чтобы в массив 'reversedNumberRow'
+// добавить те же числа, но в обратном порядке.
+const numberRow = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const reversedNumberRow = [];
+
+
+// Для каждой машины в массиве используйте "forEach"
+// что-бы вывести в консоль строку типа:
+// This is green Jaguar. It costs 7000€.
+
+const cars = [
+    {
+        make: 'BMW',
+        color: 'grey',
+        price: 10000
+    },
+    {
+        make: 'Mercedes-Benz',
+        color: 'black',
+        price: 15000
+    },
+    {
+        make: 'Honda',
+        color: 'blue',
+        price: 8000
+    },
+    {
+        make: 'Ferrari',
+        color: 'red',
+        price: 50000
+    } 
+];
